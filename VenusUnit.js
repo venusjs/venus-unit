ae8e0270c10ebbcc4a9181566896cb0e007de2e0
Seth McLaughlin <s@sethmcl.com>
2014-03-02 14:22:40 -0800

    Initial commit.

---
+++ VenusUnit.js
@@ -0,0 +1,54 @@
+var glob = require('glob'),
+    path = require('path');
+
+module.exports = VenusUnit;
+
+/**
+ * @class VenusUnit
+ * @constructor
+ * @param {Venus} venus context object
+ */
+function VenusUnit(venus, log) {
+  this.venus = venus;
+  this.log   = log;
+
+  this.bindEvents(venus);
+  this.buildTests(venus);
+};
+
+/**
+ * @method buildTests
+ * @param {Venus} venus context object
+ */
+VenusUnit.prototype.buildTests = function (venus) {
+  var tests = venus.config.tests,
+      log   = this.log;
+
+  if (!tests) {
+    log('No tests specified in config options');
+    return;
+  }
+
+  log(log.yellow(tests));
+  testPaths = glob.sync(tests).map(function (file) {
+    return path.resolve(process.cwd(), file);
+  });
+
+};
+
+/**
+ * @method bindEvents
+ * @param {Venus} venus context object
+ */
+VenusUnit.prototype.bindEvents = function (venus) {
+  var events = this.venus.events;
+
+  venus.on(events.VC_START, this.onStart.bind(this));
+};
+
+/**
+ * @method onStart
+ */
+VenusUnit.prototype.onStart = function () {
+  this.log('init venus unit, running tests:', this.venus.config.tests);
+};
