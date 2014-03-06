abc7ac1cd68a6124cdfc586e998089b3ab08fe3d
Seth McLaughlin <s@sethmcl.com>
2014-03-06 07:24:33 -0800

    WIP

--- VenusUnit.js
+++ VenusUnit.js
@@ -8,28 +8,33 @@ module.exports = VenusUnit;
  * @constructor
  * @param {Venus} venus context object
  */
-function VenusUnit(venus, log) {
+function VenusUnit(venus) {
   this.venus = venus;
-  this.log   = log;
+  this.info  = venus.info;
+  this.debug = venus.debug;
 
   this.bindEvents(venus);
   this.buildTests(venus);
 };
 
 /**
+ * @property name
+ */
+VenusUnit.prototype.name = 'venus-unit';
+
+/**
  * @method buildTests
  * @param {Venus} venus context object
  */
 VenusUnit.prototype.buildTests = function (venus) {
-  var tests = venus.config.tests,
-      log   = this.log;
+  var tests = venus.config.tests;
 
   if (!tests) {
-    log('No tests specified in config options');
+    this.info('No tests specified in config options');
     return;
   }
 
-  log(log.yellow(tests));
+  this.info(this.info.yellow(tests));
   testPaths = glob.sync(tests).map(function (file) {
     return path.resolve(process.cwd(), file);
   });
@@ -50,7 +55,7 @@ VenusUnit.prototype.bindEvents = function (venus) {
  * @method onStart
  */
 VenusUnit.prototype.onStart = function () {
-  this.log('init venus unit, running tests:', this.venus.config.tests);
+  this.info('init venus unit, running tests:', this.venus.config.tests);
   this.venus.emit('venus-http:register-namespace', 'venus-unit', this.onHttpRequest, this);
 };
 
@@ -60,6 +65,5 @@ VenusUnit.prototype.onStart = function () {
  * @param {http.Response} response
  */
 VenusUnit.prototype.onHttpRequest = function (request, response) {
-  console.log('hello from VenusUnit');
   response.end('VenusUnit is in da house');
 };
