ff8e8d5125066fd36bea1a0c6d4145f6b2dcf638
Seth McLaughlin <s@sethmcl.com>
2014-09-30 20:51:12 -0700

    Skeleton test sandbox and harness page implemented

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-module.exports = require('./VenusUnit');
+module.exports = require('./lib/VenusUnit');
---
+++ test/unit/lib/transforms/noop.spec.js
@@ -0,0 +1,23 @@
+var transform = v.lib('transforms', 'noop');
+var concat    = require('concat-stream');
+var stream    = require('stream');
+
+describe('noop', function () {
+  it('should not change value', function (done) {
+    var s, expected;
+
+    expected = 'hi';
+
+    s = new stream.Readable();
+    s.push('hi');
+    s.push(null);
+
+    s
+      .pipe(transform())
+      .pipe(concat(function (data) {
+        v.assert.equal(data.toString(), expected);
+        done();
+      }));
+  });
+});
+
